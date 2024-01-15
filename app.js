b3a7f69e9127ee2421fee5897bca279e358dc0ce
AudeNgD <aude.nguyenduc@gmail.com>
2024-01-15 14:21:51 +0000

    refactored task 2 code

--- app.js
+++ app.js
@@ -9,9 +9,23 @@ app.get("/api", getEndPoints);
 //TOPICS methods
 app.get("/api/topics", getTopics);
 
+//CUSTOM errors
+app.use((err, req, res, next) => {
+  if (err.msg && err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
 //invalid url - don't move this at the top of the app.js file
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "URL not found" });
+});
+
+//last resort error handling
+app.use((err, req, res, next) => {
+  res.status();
 });
 
 module.exports = app;
