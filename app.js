88cc5b5200d3a51ed9b93d83e3424f771df52531
AudeNgD <aude.nguyenduc@gmail.com>
2024-01-16 16:45:16 +0000

    Revert "complete setup for task 10 - get all users"
    This reverts commit 8febb01455312a8d8c6d7ceaddd9c82e3a08a9e0.

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const {
   patchVoteByArticleId,
 } = require("./controllers/articles.controllers");
 const { deleteCommentById } = require("./controllers/comments.controllers");
-const { getAllUsers } = require("./controllers/users.controllers");
-
 app.use(express.json());
 
 //CORE method
@@ -27,9 +25,6 @@ app.patch("/api/articles/:article_id", patchVoteByArticleId);
 
 //COMMENTS methods
 app.delete("/api/comments/:comment_id", deleteCommentById);
-
-//USERS methods
-app.get("/api/users", getAllUsers);
 
 //invalid syntax in SQL query
 app.use((err, req, res, next) => {
