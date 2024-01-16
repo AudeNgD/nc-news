7170dac816e584a3b6b48e883b117fdcad06167c
AudeNgD <aude.nguyenduc@gmail.com>
2024-01-16 12:44:15 +0000

    setup for task 7 - post comment

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,7 +1,9 @@
 const {
   fetchArticleById,
   fetchAllArticles,
+  addCommentByArticleId,
 } = require("../models/articles.models");
+const { checkAuthorExists } = require("../utils/check-exists");
 
 exports.getArticleById = (req, res, next) => {
   const artId = req.params.id;
@@ -19,6 +21,19 @@ exports.getArticles = (req, res, next) => {
   fetchAllArticles()
     .then((articles) => {
       res.status(200).send(articles);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postCommentByArticleId = (req, res, next) => {
+  const newComment = req.body;
+  const articleId = req.params.article_id;
+
+  addCommentByArticleId(articleId, newComment)
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
     .catch((err) => {
       next(err);
