3ac25266d51a8fe51793825d4a3f7d22bcf8afb2
AudeNgD <aude.nguyenduc@gmail.com>
2024-02-05 09:28:48 +0000

    update path to endpoints file

--- controllers/core.controllers.js
+++ controllers/core.controllers.js
@@ -1,13 +1,13 @@
 const fs = require("fs/promises");
+const file = require("../endpoints.json");
+//console.log(file);
 
 exports.getEndPoints = (req, res, next) => {
-  return fs
-    .readFile(
-      "/home/aude/northcoders/back-end/be-nc-news/endpoints.json",
-      "utf-8"
-    )
+  res.status(200).send({ endPoints: file });
+  // return fs
+  //   .readFile("../endpoints.json", "utf-8")
 
-    .then((data) => {
-      res.status(200).send({ endPoints: JSON.parse(data) });
-    });
+  //   .then((data) => {
+  //     res.status(200).send({ endPoints: JSON.parse(data) });
+  //   });
 };
